545ee2707b41b5567c3b395960182adb5508bf88
Jiaping shen <65934350+choushuiguo325@users.noreply.github.com>
2020-09-19 00:50:07 -0500

    updated the level-2 files

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -15,8 +15,6 @@ data.forEach((sighting) => {
 });
 
 var button = d3.select("#filter-btn");
-// console.log(button.text());
-// console.log("test");
 var form = d3.select("#form");
 
 form.on('submit', runEnter);
@@ -39,6 +37,57 @@ function runEnter() {
             cell.text(value);
         });
     });
-
 };
+
+var cities = [];
+var states = [];
+var countries = [];
+var shapes = [];
+
+data.forEach((sighting) => {
+    if (cities.includes(sighting.city)) {
+        ;
+    } else {
+        cities.push(sighting.city);
+    };
+
+    if (states.includes(sighting.state)) {
+        ;
+    } else {
+        states.push(sighting.state );
+    };
+
+    if (countries.includes(sighting.country)) {
+        ;
+    } else {
+        countries.push(sighting.country);
+    };
+
+    if (shapes.includes(sighting.shape)) {
+        ;
+    } else {
+        shapes.push(sighting.shape);
+    };
+});
+
+cities.sort();
+states.sort();
+countries.sort();
+shapes.sort();
+
+var formGroup = d3.select(".form-group");
+
+filtration = {"city":cities, "state":states, "country": countries, "shape": shapes};
+Object.entries(filtration).forEach(([key, value]) => {
+    var conditionGroup = formGroup.append('div');
+    conditionGroup.attr('class', 'btn-group');
+    conditionGroup.append(`<button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">find a ${key}</button>`);
+    conditionGroup.append(`<label for="date">Enter a Date</label>`);
+    var dropdownManu = conditionGroup.append('div');
+    dropdownManu.attr('class', 'dropdown-menu');
+    value.forEach(elt => {
+        dropdownManu.append(`<a class="dropdown-item" href="#">${elt}</a>`);
+    });
+
+});
 
